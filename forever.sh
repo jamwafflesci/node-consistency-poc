6857956db5b7d65b84fea84ae84574e52913fd80
James Waples <james@cambridge-intelligence.com>
2018-01-30 16:43:29 +0000

    Remove a bunch of superfluous logs

--- forever.sh
+++ forever.sh
@@ -2,10 +2,6 @@
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # Source NVM
 
-echo "$0: Using Node version $(node -v)"
-
-echo "$0: Using Forever at $(which forever)"
-
 nvm exec forever stopall
 
 nvm exec forever start index.js
--- restart.sh
+++ restart.sh
@@ -2,11 +2,7 @@
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # Source NVM
 
-echo "$0: NVMRC version: $(cat .nvmrc)"
-
 nvm install
-
-echo "$0: Using Node version $(node -v)"
 
 npm install -g yarn forever
 
